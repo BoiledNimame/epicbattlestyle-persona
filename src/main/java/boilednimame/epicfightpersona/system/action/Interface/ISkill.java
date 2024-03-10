c61d47481e1038b33bcfb79fd9304e494fd05d6d
HadukiDesktop <haduki.kyouka@gmail.com>
2024-03-11 02:12:38 +0900

    消費とか考えて;

--- src/main/java/boilednimame/epicfightpersona/system/action/Interface/ISkill.java
+++ src/main/java/boilednimame/epicfightpersona/system/action/Interface/ISkill.java
@@ -2,6 +2,7 @@ package boilednimame.epicfightpersona.system.action.Interface;
 
 import boilednimame.epicfightpersona.system.Effect;
 import boilednimame.epicfightpersona.system.Element;
+import boilednimame.epicfightpersona.system.stats.PlayerStats;
 
 import java.util.UUID;
 
@@ -9,13 +10,16 @@ public interface ISkill {
     UUID id = null;
 
     boolean isSelectable = false;
-    Element includeElement = Element.Almighty;
+    Element includeElement = null;
+
+    boolean isConsumeStats = false;
+    PlayerStats consumedStats = null;
 
     boolean isDealDamage = false;
     long baseDamage = -1;
 
     boolean isEvadable = false;
-    short baseAccuracyRate = -1;
+    int baseAccuracyRate = -1;
 
     short TargetSelf = 0;
     short TargetAlly = 1;
