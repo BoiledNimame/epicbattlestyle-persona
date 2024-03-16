971d4bc694a0afdef5d64c74aeb44fe631586a3b
HadukiLaptop <haduki.kyouka@gmail.com>
2024-03-16 18:04:38 +0900

    ステ周りはPair<K,V>に代替するAbstractMapを使用していたが本当に意味がない行為であるためHashMapへ変更 + 経験値関連を追加

--- src/main/java/boilednimame/epicfightpersona/system/stats/PersonaStats.java
+++ src/main/java/boilednimame/epicfightpersona/system/stats/PersonaStats.java
@@ -3,27 +3,65 @@ package boilednimame.epicfightpersona.system.stats;
 import boilednimame.epicfightpersona.system.element.Element;
 import boilednimame.epicfightpersona.system.element.ElementAffinity;
 
-import java.util.AbstractMap;
+import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class PersonaStats implements ElementAffinity {
+    // RefinedStorageのDiskにもuuidついてるしそれ見ような！^^
+    private final UUID uuid = UUID.randomUUID();
+    private int level;
+    private int nextExp;
+    private static final int MAX_LEVEL = 99;
+    private final Map<PersonaBaseStats, Integer> baseStatsMap;
     private final Element[][] elementsAffinity = new Element[ABSORB+1][];
 
-    AbstractMap.SimpleEntry<PersonaBaseStats, Integer> STR = new AbstractMap.SimpleEntry<>(PersonaBaseStats.STR, -1);
-    AbstractMap.SimpleEntry<PersonaBaseStats, Integer> MAG = new AbstractMap.SimpleEntry<>(PersonaBaseStats.MAG, -1);
-    AbstractMap.SimpleEntry<PersonaBaseStats, Integer> END = new AbstractMap.SimpleEntry<>(PersonaBaseStats.END, -1);
-    AbstractMap.SimpleEntry<PersonaBaseStats, Integer> AGI = new AbstractMap.SimpleEntry<>(PersonaBaseStats.AGI, -1);
-    AbstractMap.SimpleEntry<PersonaBaseStats, Integer> LUK = new AbstractMap.SimpleEntry<>(PersonaBaseStats.LUK, -1);
-
-    public PersonaStats(int str, int ma, int ed, int agi, int luk, Map<Element, Integer> map) {
-        this.STR.setValue(str);
-        this.MAG.setValue(ma);
-        this.END.setValue(ed);
-        this.AGI.setValue(agi);
-        this.LUK.setValue(luk);
+    public PersonaStats(int level, int exp, int str, int ma, int ed, int agi, int luk, Map<Element, Integer> map) {
+        this.level = level;
+        this.nextExp = exp;
+        this.baseStatsMap = new EnumMap<PersonaBaseStats, Integer>(PersonaBaseStats.class){{
+            put(PersonaBaseStats.STR, str);
+            put(PersonaBaseStats.MAG, ma);
+            put(PersonaBaseStats.END, ed);
+            put(PersonaBaseStats.AGI, agi);
+            put(PersonaBaseStats.LUK, luk);
+        }};
         for (int i = 0; i<ABSORB+1 ;i++) {
             elementsAffinity[i] = getElements(map, i);
         }
+    }
+
+    public void addExp(int gainedExp) {
+        if (!(level <= MAX_LEVEL)) {
+            LEVEL_UP: {
+                int consumedExp = gainedExp;
+                for (;;) {
+                    if (nextExp <= consumedExp) {
+                        consumedExp = consumedExp - nextExp;
+                        level++;
+                        /*
+                         * おおよそx^2だが少しそれより大きい
+                         * 主人公のLv.UP要求Expより体感低い気がしてるんで, 少し低くした
+                         * もっと高くしてもいいかもしれない -> 絞首刑etc...
+                         * ここの定数を弄る場合はGeoGebraを使ってテストするとよい.
+                         */
+                        nextExp = 20 + (level/6)*(3*level + 1) * (2*level + 1);
+                    } else {
+                        if (level == MAX_LEVEL) {
+                            nextExp = 0;
+                        } else {
+                            nextExp = nextExp - consumedExp;
+                        }
+                        break LEVEL_UP;
+                    }
+                }
+            }
+        }
+    }
+
+    public Map<PersonaBaseStats, Integer> getStats() {
+        return baseStatsMap;
     }
 
     private Element[] getElements(Map<Element, Integer> elements, int affinity) {
@@ -34,6 +72,11 @@ public class PersonaStats implements ElementAffinity {
                 .toArray(new Element[0]);
     }
 
+    /**
+     * get this persona's Element.
+     * @param affinity use {@link boilednimame.epicfightpersona.system.element.ElementAffinity}
+     * @return an Element belonging to the specified affinity.
+     */
     public Element[] getElements(int affinity) {
         if (affinity<=elementsAffinity.length) {
             return elementsAffinity[affinity];
